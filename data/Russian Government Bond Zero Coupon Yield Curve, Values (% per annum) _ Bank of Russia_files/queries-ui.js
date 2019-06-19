997f204feaf032f722c840f3b628dea0d6840d93
Marcel Dietsch <dietsch@gmail.com>
2019-06-19 15:47:16 +0200

    major cleanup and improvement of charts

---
+++ data/Russian Government Bond Zero Coupon Yield Curve, Values (% per annum) _ Bank of Russia_files/queries-ui.js
@@ -0,0 +1,142 @@
+﻿var cbr = cbr || {};
+cbr.modules = cbr.modules || {};
+cbr.modules.queries = cbr.modules.queries || {};
+cbr.modules.queries.uniDb = (function()
+{
+    var calculateDatePickerYearRange = function (minDate, maxDate) {
+        if (!minDate && !maxDate)
+            return undefined;
+
+        var range = "";
+        if (minDate) {
+            range += $.datepicker.formatDate("yy", minDate);
+        }
+        else {
+            range += "c-10"
+        };
+        range += ":";
+        if (maxDate) {
+            range += $.datepicker.formatDate("yy", maxDate);
+        }
+        else {
+            range += "c+10"
+        };
+        return range;
+    };
+
+    return {
+        showDateTimePickerForCalendarParam: function(dates, id, datepickerId) {
+            var inputWithDate = $('#' + id);
+            var currentdate = inputWithDate.val();
+            var $datepickerSelector = $('#' + datepickerId);
+            var minDate;
+            var maxDate;
+            $.each(dates, function (i, d) {
+                if (!minDate || minDate > d) {
+                    minDate = d;
+                }
+                if (!maxDate || maxDate < d) {
+                    maxDate = d;
+                }
+            });
+            var range;
+            if (minDate && maxDate) {
+                range = "" + $.datepicker.formatDate("yy", minDate) + ":" + $.datepicker.formatDate("yy", maxDate);
+            }
+
+            var dTimes = $.map(dates, function (e) { return e.getTime(); });
+            $datepickerSelector.datepicker({
+                changeMonth: true,
+                changeYear: true,
+                showButtonPanel: false,
+                minDate: minDate,
+                maxDate: maxDate,
+                yearRange: calculateDatePickerYearRange(minDate, maxDate),
+                beforeShowDay: function (date) {
+                    if ($.inArray(date.getTime(), dTimes) != -1) {
+                        return [true];
+                    }
+                    return [false];
+                },
+                onSelect: function (dateText, inst) {
+                    inputWithDate.val(dateText).trigger('change');
+                }
+            });
+            $datepickerSelector.datepicker("setDate", currentdate);
+        },
+        initCalendarDatePicker: function(id, minDate, maxDate) {
+            var $selector = $('#' + id);
+
+            $selector.datepicker({
+                changeMonth: true,
+                changeYear: true,
+                minDate: minDate,
+                maxDate: maxDate,
+                yearRange: calculateDatePickerYearRange(minDate, maxDate)
+            });
+        }
+    };
+})();
+
+cbr.modules.queries.createEditor = function (id, linesCount, mode, floating) {
+    var mode = mode || 'ace/mode/sql';
+    var linesCount = linesCount || 10;
+    var editor = ace.edit(id);
+    setTimeout(function () {
+        editor.session.setMode(mode);
+        editor.setTheme('ace/theme/github');
+        editor.setOptions({
+
+            autoScrollEditorIntoView: true,
+            maxLines: linesCount,
+            minLines: linesCount
+        });
+        if (floating) {
+            editor.on("focus", function () {
+                var linesCountOnFocus = linesCount * 2;
+                editor.setOption("minLines", linesCountOnFocus);
+                editor.setOption("maxLines", linesCountOnFocus);
+            });
+            editor.on("blur", function () {
+                editor.setOption("minLines", linesCount);
+                editor.setOption("maxLines", linesCount);
+            });
+        }
+    }, 0);
+    return editor;
+};
+cbr.modules.queries.getEditorValue = function (editor) {
+    if (editor) {
+        var value = editor.getSession().getValue();
+        return value;
+    }
+};
+cbr.modules.queries.addParentHrefToAnchorAsParameter = function (anchor) {
+    try {
+        var parentHref = parent.location.href;
+        var documentHref = document.location.href;
+        if (parentHref === documentHref)
+        {
+            return true;
+        }
+        var anchorHref = anchor.href.trim();
+        //we can not use URL bject beacuse it does not support in IE
+        var existParameters = (anchorHref.indexOf('?')!==-1)&&((anchorHref.indexOf('?') + 1) < anchorHref.length);
+        if (existParameters) {
+            anchorHref = anchorHref + '&';
+        }
+        else
+        {
+            if (anchorHref.indexOf('?') === -1)
+            {
+                anchorHref = anchorHref + '?';
+            }
+        }
+        anchor.href = anchorHref + 'parentHref=' + encodeURIComponent(parentHref);
+    }
+    catch(e)
+    {
+        //blocked a frame with origin "frame href" from accessing a cross-origin frame.
+    }
+    return true;
+}
---
+++ data/Russian Government Bond Zero Coupon Yield Curve, Values (% per annum) _ Bank of Russia_files/site.js
@@ -0,0 +1,102 @@
+﻿$(function () {
+    function scrollToIfNeed(y) {
+        var space = 120;
+        y = y - space;
+        if (y < 0) y = 0;
+
+        var scrollTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        if (!(scrollTop <= y && y < scrollTop + windowHeight - 2 * space)) {
+            if (scrollTop + windowHeight - 2 * space <= y && y < scrollTop + windowHeight) {
+                y = scrollTop + (2 * space - (scrollTop + windowHeight - y));
+            }
+            window.scrollTo(0, y);
+        }
+    }
+
+    var $anchorElement;
+    function highlightAnchor() {
+        try {
+            $(window).unbind('hashchange.highlightAnchor');
+
+            var anchor = window.location.hash;
+            if (anchor && $anchorElement && $(anchor).get(0) === $anchorElement.get(0)) return;
+            if ($anchorElement) {
+                $anchorElement.removeClass('yellow');
+                $anchorElement = null;
+            }
+            if (anchor && /^#[a-z0-9-_]+$/i.test(anchor)) {
+                var $elt = $(anchor);
+                if ($elt.length !== 0) {
+                    $anchorElement = $elt;
+                    if ($elt.is('.referenceable')) {
+                        $anchorElement.addClass('yellow');
+                    }
+                    $elt.parents().andSelf().trigger('cms-expand');
+                    if (anchor !== window.location.hash) {
+                        var from = window.location.hash;
+                        window.location.hash = anchor;
+                    }
+                    setTimeout(function () { scrollToIfNeed($elt.offset().top); }, 200);
+                }
+            }
+        }
+        finally {
+            $(window).bind('hashchange.highlightAnchor', highlightAnchor);
+        }
+    }
+    setTimeout(highlightAnchor, 0);
+
+    $.fn.replaceText = function (search, replace, text_only) {
+        return this.each(function () {
+            var node = this.firstChild,
+                val,
+                new_val,
+                remove = [];
+            if (node) {
+                do {
+                    if (node.nodeType === 3) {
+                        val = node.nodeValue;
+                        new_val = val.replace(search, replace);
+                        if (new_val !== val) {
+                            if (!text_only && /</.test(new_val)) {
+                                $(node).before(new_val);
+                                remove.push(node);
+                            } else {
+                                node.nodeValue = new_val;
+                            }
+                        }
+                    }
+                    else if (node.nodeType === 1) {
+                        $(node).replaceText(search, replace, text_only);
+                    }
+                } while (node = node.nextSibling);
+            }
+            remove.length && $(remove).remove();
+        });
+    };
+
+    function highlightSearch() {
+        try {
+            $(window).unbind('hashchange.highlightSearch');
+
+            if (location.hash && location.hash.indexOf('#highlight=') === 0) {
+                $('mark').each(function () { $(this).replaceWith($(this).html()); });
+                var wordsPattern = decodeURIComponent(location.hash.split('=')[1]);
+                if (/^[a-zа-яё0-9_\-]+(\|[a-zа-яё0-9_\-]+)*$/gi.exec(wordsPattern)) {
+                    $('#content').replaceText(new RegExp('(^|[^a-zа-яё0-9_\\-])(' + wordsPattern + ')(?=$|[^a-zа-яё0-9_\\-])', 'gi'), '$1<mark>$2</mark>');
+                }
+                var $marks = $('mark').trigger('cms-expand');
+                if ($marks.length) {
+                    setTimeout(function () { scrollToIfNeed($marks.offset().top); }, 200);
+                }
+            }
+        }
+        finally {
+            $(window).bind('hashchange.highlightSearch', highlightSearch);
+        }
+    }
+    setTimeout(highlightSearch, 0);
+
+});
